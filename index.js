46a5e72946f7a33ff1d9854aa5217e05f6517a74
Tamarie <geronimojeffries@gmail.com>
2025-08-10 21:21:35 -0500

    responsiveness

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-// template_0el68yp
-//service_fqzdtwo
-//-njBJj0ZYPf982s61
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-function contact(event) {
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isodd = i % 2 !== 0;
+        const boolInt = isodd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+   
+    function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
@@ -22,4 +45,14 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly at geronimojeffries@gmail.com."
             );
         })
+    }
+
+    let t = false;
+    function toggleModal() {
+        if (isModalOpen) {
+            isModalOpen = false;
+            return document.body.classList.remove("modal--open");
+        }
+        isModalOpen = true;
+        document.body.classList += " modal--open";
     }
